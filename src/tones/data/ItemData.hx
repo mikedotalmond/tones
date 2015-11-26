0d869fc5ba409710616ffd423965bc38c4334cce
mike <mikedotalmond@googlemail.com>
2015-11-26 02:22:01 +0000

    ItemSrcNode typedef for easier referencing

--- src/tones/data/ItemData.hx
+++ src/tones/data/ItemData.hx
@@ -10,9 +10,11 @@ import js.html.audio.AudioNode;
 import js.html.audio.OscillatorNode;
 import js.html.audio.AudioBufferSourceNode;
 
+typedef ItemSrcNode = haxe.extern.EitherType<AudioBufferSourceNode, OscillatorNode>;
+
 typedef ItemData = {
 	var id:Int;
-	var src:haxe.extern.EitherType<AudioBufferSourceNode, OscillatorNode>;
+	var src:ItemSrcNode;
 	var env:GainNode;
 	var triggerTime:Float;
 	var attack:Float;
